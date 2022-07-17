ca5a7917a3db017e490418536e89c82697fdf777
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-07-17 12:17:50 +0700

    08 Depedency Injection

--- latihan_bloc/lib/main.dart
+++ latihan_bloc/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:latihan_bloc/bloc/counter.dart';
 import 'package:latihan_bloc/ui/pages/home.dart';
-import 'package:latihan_bloc/ui/pages/home1.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(const MyApp());
 }
 
@@ -18,106 +15,8 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (context) => Counter(),
       child: MaterialApp(
-        home: HomePage1(),
+        home: HomePage(),
       ),
     );
   }
 }
-
-
-// import 'package:bloc/bloc.dart';
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: HomePage(),
-//     );
-//   }
-// }
-
-// class CounterCubit extends Cubit<int> {
-//   CounterCubit({this.inisialData = 0}) : super(inisialData);
-//   int inisialData;
-//   // int? current;
-//   // int? next;
-
-//   void tambahData() {
-//     emit(state + 1); //yield package dari sananya/package bloc
-//   }
-
-//   void kurangData() {
-//     emit(state - 1);
-//   }
-// //bloc punya fitur untuk kita bisa memantau suatu aplikasinya dengan materi
-//   //Observer
-//   //-perubahan (onChange)
-//   // -error (on error)
-
-//   @override
-//   void onChange(Change<int> change) {
-//     super.onChange(change);
-//     print(change);
-//     // current = change.currentState; //untuk mau ditampilkan diwidget
-//     // next = change.nextState;
-//   }
-// //bloc juga punya untuk mengecek error
-//   @override
-//   void onError(Object error, StackTrace stackTrace) {
-//     super.onError(error, stackTrace);
-//     print(error);
-//   }
-// }
-
-// class HomePage extends StatelessWidget {
-//   CounterCubit mycounter = CounterCubit(inisialData: 0);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Cubit Apps"),
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           StreamBuilder(
-//               initialData: mycounter.inisialData,
-//               stream: mycounter.stream,
-//               builder: (context, snapshot) {
-//                 return Center(
-//                   child: Text(
-//                     "${snapshot.data}",
-//                     style: const TextStyle(
-//                       fontSize: 50,
-//                     ),
-//                   ),
-//                 );
-//               }),
-//           const SizedBox(height: 20),
-//           Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-//             IconButton(
-//               onPressed: () {
-//                 mycounter.kurangData();
-//               },
-//               icon: const Icon(Icons.remove),
-//             ),
-//             IconButton(
-//               onPressed: () {
-//                 mycounter.tambahData();
-//               },
-//               icon: const Icon(Icons.add),
-//             ),
-//           ])
-//         ],
-//       ),
-//     );
-//   }
-// }
--- latihan_bloc/lib/ui/pages/home.dart
+++ latihan_bloc/lib/ui/pages/home.dart
@@ -1,114 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:latihan_bloc/bloc/counter.dart';
+import 'package:latihan_bloc/ui/widgets/merah.dart';
 
 class HomePage extends StatelessWidget {
-  Counter mycounter = Counter();
-
   @override
   Widget build(BuildContext context) {
+    // Counter mycounter = BlocProvider.of<Counter>(context);
+    Counter mycounter = context.read<Counter>();
+
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("BLOC Consumer"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BlocConsumer<Counter, int>(
-            bloc: mycounter,
-            buildWhen: (previous, current) {
-              if (current >= 1) {
-                return true;
-              } else {
-                return false;
-              }
-            },
-            builder: (context, state) {
-              return Text(
-                "$state",
-                style: const TextStyle(
-                  fontSize: 50,
-                ),
-              );
-            },
-            listener: (context, state) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text("Data Genap"),
-                ),
-              );
-            },
-            listenWhen: (previous, current) {
-              if (current % 2 == 0) {
-                return true;
-              } else {
-                return false;
-              }
-            },
-          ),
-          // BlocListener<Counter, int>(
-          //   bloc: mycounter,
-          //   listener: (context, state) {
-          //     ScaffoldMessenger.of(context).showSnackBar(
-          //       SnackBar(
-          //         content: Text("Data Genap"),
-          //       ),
-          //     );
-          //   },
-          //   listenWhen: (previous, current) {
-          //     if (current % 2 == 0) {
-          //       return true;
-          //     } else {
-          //       return false;
-          //     }
-          //   },
-          //   child: BlocBuilder<Counter, int>(
-          //       bloc: mycounter,
-          //       // buildWhen: (previous, current) {
-          //       //   if (current % 2 == 1) {
-          //       //     return true;
-          //       //   } else {
-          //       //     return false;
-          //       //   }
-          //       // },
-          //       builder: (context, state) {
-          //         return Text(
-          //           "$state",
-          //           style: const TextStyle(
-          //             fontSize: 50,
-          //           ),
-          //         );
-          //       }),
-          // ),
-          // StreamBuilder(
-          //     initialData: mycounter.init,
-          //     stream: mycounter.stream,
-          //     builder: (context, snapshot) {
-          //       return Text(
-          //         "${snapshot.data}",
-          //         style: const TextStyle(
-          //           fontSize: 50,
-          //         ),
-          //       );
-          //     }),
-          const SizedBox(height: 50),
-          Row(
+        appBar: AppBar(
+          title: const Text("DEPENDENCY INJECTION"),
+        ),
+        body: Center(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              IconButton(
-                  onPressed: () {
+              Material(
+                color: Colors.green,
+                child: InkWell(
+                  onTap: () {
+                    //decrement
                     mycounter.decrement();
                   },
-                  icon: const Icon(Icons.remove)),
-              IconButton(
-                  onPressed: () {
+                  child: SizedBox(
+                    height: 70,
+                    width: 70,
+                    child: Icon(
+                      Icons.remove,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Merah(),
+              Material(
+                color: Colors.green,
+                child: InkWell(
+                  onTap: () {
+                    //increment
                     mycounter.increment();
                   },
-                  icon: const Icon(Icons.add)),
+                  child: SizedBox(
+                    height: 70,
+                    width: 70,
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
-        ],
-      ),
-    );
+        ),
+      );
   }
 }
